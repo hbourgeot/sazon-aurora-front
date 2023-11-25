dd612f1c2c347c4670e77bbe9d923873782ecb03
Henrry Bourgeot <henrrybrgt@gmail.com>
2023-11-25 09:46:45 -0400

    starting global view

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import Sidebar from "$lib/components/Sidebar.svelte";
   import "../app.pcss";
   import { Button, Drawer, Layout, Portal } from "stwui";
   import { Icon, Bars3 } from "svelte-hero-icons";
@@ -6,26 +7,20 @@
   function openDrawer() {
     drawerOpen = true;
   }
-
-  function closeDrawer() {
-    drawerOpen = false;
-  }
 </script>
 
 <Layout class="h-full">
-  <Layout.Header class="static z-0">
+  <Layout.Header class="static z-0 px-3">
     <Button type="primary" on:click="{openDrawer}">
       <Icon src="{Bars3}" class="h-5 w-5" />
     </Button>
-    <Layout.Header.Extra slot="extra">Extra</Layout.Header.Extra>
+    <Layout.Header.Extra slot="extra">
+      <h1 class="text-3xl">La sazón de Aurora</h1>
+    </Layout.Header.Extra>
   </Layout.Header>
   <Layout.Content>
     <Layout.Content.Body class="flex items-center justify-center min-h-[200px]">
-      <Portal>
-        {#if drawerOpen}
-          <Drawer handleClose={closeDrawer} placement="left" />
-        {/if}
-      </Portal>
+      <Sidebar open={drawerOpen}/>
       <slot />
     </Layout.Content.Body>
   </Layout.Content>
