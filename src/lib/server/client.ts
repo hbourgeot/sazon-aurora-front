0a912a36fe8ecc648611fc3584fc4d87da1802d2
Henrry Bourgeot <henrrybrgt@gmail.com>
2023-11-29 21:31:29 -0400

    using svelxios

---
+++ src/lib/server/client.ts
@@ -0,0 +1,33 @@
+
+import {baseUrl} from "$env/static/private"
+import axios from "axios"
+import {debugResponse, debugRequest} from "$lib/utils/debugger"
+
+const client = axios.create({
+  baseURL: baseUrl,
+});
+
+client.interceptors.response.use(
+  (response) => {
+    debugResponse(response);
+    return response;
+  },
+  (error) => {
+    debugResponse(error.response, error);
+    return Promise.reject(error);
+  }
+);
+
+client.interceptors.request.use(
+  (config) => {
+    debugRequest(config);
+    return config;
+  },
+  (error) => {
+    debugRequest(error.config);
+    return Promise.reject(error);
+  }
+)
+
+export {client}
+  
