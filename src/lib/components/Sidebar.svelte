f415e868828d4cfae522a440030a2e517f92fbc1
Henrry Bourgeot <henrrybrgt@gmail.com>
2023-12-09 13:30:57 -0400

    adding more options to sidebar

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -1,22 +1,42 @@
 <script lang="ts">
-  import { Drawer, List, Portal } from "stwui";
+  import { Avatar, Badge, Button, Drawer, Dropdown, List, Portal } from "stwui";
+  import { EllipsisHorizontal, Icon, Trash } from "svelte-hero-icons";
 
   export let open: boolean = false;
   const handleClose: () => void = () => {
     open = false;
   };
 
-  const items = [
+  const menuItems = [
     {
-      title: "Calvin Hawkins",
+      title: "Nuestro menú",
     },
     {
-      title: "Kristen Ramos",
+      title: "Ver tu carrito",
     },
     {
-      title: "Ted Fox",
+      title: "Sobre nosotros",
     },
   ];
+
+  const items = [
+    {
+      avatar:
+        "https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+      title: "Calvin Hawkins",
+      description: "calvin.hawkins@example.com",
+    },
+  ];
+
+  let visible2 = false;
+
+  function closeDropdown2() {
+    visible2 = false;
+  }
+
+  function toggleDropdown2() {
+    visible2 = !visible2;
+  }
 </script>
 
 <Portal>
@@ -27,7 +47,7 @@
       </Drawer.Header>
       <Drawer.Content>
         <List>
-          {#each items as item}
+          {#each menuItems as item}
             <List.Item>
               <List.Item.Content slot="content">
                 <List.Item.Content.Description class="w-full" slot="title"
@@ -38,6 +58,54 @@
           {/each}
         </List>
       </Drawer.Content>
+      <Drawer.Footer>
+        <List>
+          {#each items as item}
+            <List.Item>
+              <List.Item.Leading slot="leading">
+                <List.Item.Leading.Avatar slot="avatar" src="{item.avatar}" />
+              </List.Item.Leading>
+              <List.Item.Content slot="content" class="relative">
+                <List.Item.Content.Title slot="title"
+                  >{item.title}</List.Item.Content.Title
+                >
+                <List.Item.Content.Description slot="description">
+                  {item.description}
+                </List.Item.Content.Description>
+              </List.Item.Content>
+
+              <Dropdown bind:visible="{visible2}">
+                <Button
+                  slot="trigger"
+                  shape="circle"
+                  type="ghost"
+                  on:click="{toggleDropdown2}"><Icon src={EllipsisHorizontal} class="h-5 w-5"/></Button
+                >
+                <Dropdown.Items slot="items" alignment="center">
+                  <Dropdown.Items.Item
+                    on:click="{closeDropdown2}"
+                    label="Item 1"
+                  ></Dropdown.Items.Item>
+                  <Dropdown.Items.Item
+                    on:click="{closeDropdown2}"
+                    label="Item 2"
+                  ></Dropdown.Items.Item>
+                  <Dropdown.Items.Item
+                    on:click="{closeDropdown2}"
+                    label="Notifications"
+                  >
+                    <Badge type="info" slot="extra">+99</Badge>
+                  </Dropdown.Items.Item>
+                  <Dropdown.Items.Divider />
+                  <Button type="danger" class="w-full justify-between">
+                    Home
+                  </Button>
+                </Dropdown.Items>
+              </Dropdown>
+            </List.Item>
+          {/each}
+        </List>
+      </Drawer.Footer>
     </Drawer>
   {/if}
 </Portal>
