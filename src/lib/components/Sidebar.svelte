43a20bd720f6cb81ddee1f2fb57060f22ecacbf3
Henrry Bourgeot <henrrybrgt@gmail.com>
2023-12-09 09:25:20 -0400

    modifing sidebar

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -1,10 +1,22 @@
 <script lang="ts">
-  import { Drawer, Portal } from "stwui";
+  import { Drawer, List, Portal } from "stwui";
 
   export let open: boolean = false;
   const handleClose: () => void = () => {
     open = false;
   };
+
+  const items = [
+    {
+      title: "Calvin Hawkins",
+    },
+    {
+      title: "Kristen Ramos",
+    },
+    {
+      title: "Ted Fox",
+    },
+  ];
 </script>
 
 <Portal>
@@ -13,6 +25,19 @@
       <Drawer.Header>
         <h2 class="text-3xl">Menú</h2>
       </Drawer.Header>
+      <Drawer.Content>
+        <List>
+          {#each items as item}
+            <List.Item>
+              <List.Item.Content slot="content">
+                <List.Item.Content.Description class="w-full" slot="title"
+                  ><a href="/" class="w-full block">{item.title}</a>
+                </List.Item.Content.Description>
+              </List.Item.Content>
+            </List.Item>
+          {/each}
+        </List>
+      </Drawer.Content>
     </Drawer>
   {/if}
 </Portal>
