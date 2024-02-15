f03df0655b034f514a3e9e53199a67b43bb6aedc
Henrry Bourgeot <henrrybrgt@gmail.com>
2024-02-15 10:05:20 -0400

    more assets

--- src/lib/assets/index.ts
+++ src/lib/assets/index.ts
@@ -5,4 +5,5 @@ import login from "./login.webp";
 import barGraph from "./bar-graph.png";
 import dollar from "./dollar.svg";
 import empty from "./empty.webp";
-export { burger, cachapa, cesar, login, barGraph, dollar, empty };
+import emptyCart from "./empty-cart.webp";
+export { burger, cachapa, cesar, login, barGraph, dollar, empty, emptyCart };
